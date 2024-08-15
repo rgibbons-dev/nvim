f72f651c3330597e80a47d4d814d97b330fd2506
ryan gibbons <amalgam.drawers.0k@icloud.com>
2024-08-15 11:46:35 -0400

    linux setup

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.g.have_nerd_font = true
+vim.g.have_nerd_font = false
 vim.opt.number = true
 vim.opt.mouse = "a"
 vim.opt.showmode = false
