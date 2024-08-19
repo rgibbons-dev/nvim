8b126ef11200962d27feae1e8f87bfe4a6c5bd96
ryan gibbons <amalgam.drawers.0k@icloud.com>
2024-08-19 09:46:10 -0400

    fixed gitsync config

--- lua/plugins/gitsync.lua
+++ lua/plugins/gitsync.lua
@@ -1,7 +1,9 @@
 return {
     "gitsync.nvim",
-    name = "gitsync.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     dir = "~/.local/code/local/gitsync.nvim",
     dev = { true },
+    config = function()
+        require("gitsync").check_remote("/Users/ryan/.config/nvim")
+    end,
 }
