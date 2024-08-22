965a69afa9a2b3762cd87f179509ae69172214ad
ryan gibbons <amalgam.drawers.0k@icloud.com>
2024-08-21 20:03:19 -0400

    moved gitsync from loc to gh

--- lua/plugins/gitsync.lua
+++ lua/plugins/gitsync.lua
@@ -1,8 +1,6 @@
 return {
-    "gitsync.nvim",
+    "rgibbons-dev/gitsync.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    dir = "~/.local/code/local/gitsync.nvim",
-    dev = { true },
     config = function()
         require("gitsync").check_remote("/Users/ryan/.config/nvim")
     end,
