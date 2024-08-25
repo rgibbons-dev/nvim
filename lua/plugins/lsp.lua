28bb2eefc036be009065dc6d56f7e530a54dcd1a
ryan gibbons <amalgam.drawers.0k@icloud.com>
2024-08-25 13:54:10 -0400

    rm clang

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -62,7 +62,6 @@ return {
         capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
         local servers = {
-            clangd = {},
             lua_ls = {},
         }
 
