9842d63db05ff9e8bf194548eef92e7890c65267
Ryan Gibbons <amalgam.drawers.0k@icloud.com>
2024-08-31 15:01:49 -0400

    added comment to test gitcommit grammar

--- init.lua
+++ init.lua
@@ -14,6 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- kickstart options
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = false
