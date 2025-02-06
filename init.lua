02ebdbcb7f7afa5f508b94cefb2ef0135bd3e70e
Ryan Gibbons <amalgam.drawers.0k@icloud.com>
2025-02-05 22:28:30 -0500

    fix deprecated goto_next

--- init.lua
+++ init.lua
@@ -43,8 +43,9 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+-- 8ba73f0e4cc6c82032a348a1d6c8d794ed150fd7 deprecated vim.diagnostic.goto_next in favor of jump
+vim.keymap.set("n", "[d", vim.diagnostic.jump, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.jump, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
